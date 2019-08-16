44bf64ac2b4904ffa08d466fecfe5b27ef7e6308
Nidal Omar Bakir <nedalbaker2000@gmail.com>
2019-08-17 02:59:41 +0300

    add dependencies

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -2,14 +2,56 @@ package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI.IdpConfig;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth=FirebaseAuth.getInstance();
+    private static final int RESULT_CODE = 757;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.sign_bt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               IdpConfig phoneConfigWithDefaultNumber = new AuthUI.IdpConfig.PhoneBuilder()
+                        .setDefaultCountryIso("sy")
+                        .build();
+              startActivityForResult(AuthUI
+                      .getInstance()
+                      .createSignInIntentBuilder()
+                      .setAvailableProviders(Arrays.asList(phoneConfigWithDefaultNumber))
+                      .setTosAndPrivacyPolicyUrls(
+                        "https://joebirch.co/terms.html"
+                              ,"https://joebirch.co/privacy.html")
+                      .build(),RESULT_CODE);
+            }
+        });
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (auth.getCurrentUser()!=null) {
+            finish();
+            startActivity(new Intent(MainActivity.this,baseChatActivity.class));
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/whatsappclone/UserSettings.java
@@ -0,0 +1,52 @@
+package com.example.whatsappclone;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserSettings {
+    private FirebaseAuth firebaseAuth;
+
+    public UserSettings() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    public static Boolean isSignIn() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        return firebaseAuth.getCurrentUser() == null ? false : true;
+    }
+
+    public static void signOut(final Activity activity, Context context) {
+        AuthUI.getInstance().signOut(context).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                activity.startActivity(new Intent(activity, MainActivity.class));
+                activity.finish();
+            }
+        });
+    }
+    public static void deleteUser(final Activity activity, Context context){
+            String UID= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        AuthUI.getInstance().delete(context).addOnCompleteListener( new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                //ToDO:delete the user chats and all subCollections
+                activity.startActivity(new Intent(activity,MainActivity.class));
+                activity.finish();
+
+            }
+        });
+
+    }
+
+}
+
