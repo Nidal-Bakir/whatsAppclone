31fddecff7296b2a8d4c3a4de762aed519efadf3
Nidal Omar Bakir <nedalbaker2000@gmail.com>
2019-08-18 01:49:06 +0300

    add some comments

--- app/src/main/java/com/example/whatsappclone/BaseChatActivity2.java
+++ app/src/main/java/com/example/whatsappclone/BaseChatActivity2.java
@@ -103,15 +103,13 @@ public class BaseChatActivity2 extends AppCompatActivity implements NavigationVi
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_newGroup) {
+            //ToDO:handel the new group action
+        } else if (id == R.id.nav_contacts) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
+        }  else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
